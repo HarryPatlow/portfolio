b53465baa1951b47483ac0611fe3213f10b71c89
Harry Patlow <ghp.coding@gmail.com>
2022-08-30 10:40:58 -0400

    Add Menu Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,256 @@
 import './App.css';
+import React from "react";
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import {
+  Bars3Icon,
+  BookmarkSquareIcon,
+  CalendarIcon,
+  AcademicCapIcon,
+  LifebuoyIcon,
+  ShieldCheckIcon,
+  CodeBracketIcon,
+  XMarkIcon,
+  PuzzlePieceIcon,
+  MegaphoneIcon,
+  BuildingOffice2Icon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+const portfolio = [
+  {
+    name: 'Academic',
+    description: 'Here\'s a couple of stuffs about all my work and studies at school',
+    href: '#',
+    icon: AcademicCapIcon,
+  },
+  {
+    name: 'Coding Skills',
+    description: 'Being a Web/App Developer means mastering a bunch of Abilities',
+    href: '#',
+    icon: CodeBracketIcon,
+  },
+  { name: 'Extra Curricular', 
+    description: "Apart from Academics, there are a few things I'm Good at", 
+    href: '#', 
+    icon: PuzzlePieceIcon },
+  {
+    name: 'Accomplishments ',
+    description: "Here I sum up all my accomplishments in a Wide Range of Topics",
+    href: '#',
+    icon: MegaphoneIcon,
+  },
+  {
+    name: 'My Startup',
+    description: 'This page focuses on my startup during my schooldays on Blogging Site',
+    href: '#',
+    icon: BuildingOffice2Icon,
+  },
+]
+const resources = [
+  {
+    name: 'Help Center',
+    description: 'Get all of your questions answered in our forums or contact support.',
+    href: '#',
+    icon: LifebuoyIcon,
+  },
+  {
+    name: 'Guides',
+    description: 'Learn how to maximize our platform to get the most out of it.',
+    href: '#',
+    icon: BookmarkSquareIcon,
+  },
+  {
+    name: 'Events',
+    description: 'See what meet-ups and other events we might be planning near you.',
+    href: '#',
+    icon: CalendarIcon,
+  },
+  { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#', icon: ShieldCheckIcon },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 
 function App() {
+  
+  //JS CODE   
+  const activeMenu = "text-base font-medium text-gray-900 hover:text-gray-1000 border-b-4 border-indigo-700"
+
+  const inactiveMenu = "text-base font-medium text-gray-500 hover:text-gray-900"
+
+  //TEMPLATE CODE
   return (
-    <div className="App">
-      hey there
-    </div>
-  );
+    <Popover className="relative bg-white">
+      <div className="mx-auto max-w-7xl px-6 sm:px-16">
+        <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <p href="#">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-20 w-auto sm-16"
+                src="https://cdn.dribbble.com/users/2592071/screenshots/7327591/logo-artboard_2_4x_4x.png"
+                alt=""
+              />
+            </p>
+          </div>
+          <div className="-my-2 -mr-2 md:hidden">
+            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Open menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+            <button className={activeMenu}>
+                Home
+            </button>
+            <button className={inactiveMenu}>
+              About
+            </button>
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    className={classNames(
+                      open ? 'text-gray-900' : 'text-gray-500',
+                      'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                    )}
+                  >
+                    <span>Portfolio</span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        open ? 'text-gray-600' : 'text-gray-400',
+                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
+                      <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                          {portfolio.map((item) => (
+                            <a
+                              key={item.name}
+                              href={item.href}
+                              className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                            >
+                              <item.icon className="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" />
+                              <div className="ml-4">
+                                <p className="text-base font-medium text-gray-900">{item.name}</p>
+                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+                              </div>
+                            </a>
+                          ))}
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+            
+            <p className={inactiveMenu}>
+              Projects
+            </p>
+            <p className="text-base font-medium text-gray-500 hover:text-gray-900">
+              Contact
+            </p>
+
+            
+          </Popover.Group>
+        </div>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
+          <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="px-5 pt-5 pb-6">
+              <div className="flex items-center justify-between">
+                <div>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
+                    alt="Workflow"
+                  />
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="mt-6">
+                <nav className="grid gap-y-8">
+                  {portfolio.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
+                    >
+                      <item.icon className="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" />
+                      <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                    </a>
+                  ))}
+                </nav>
+              </div>
+            </div>
+            <div className="space-y-6 py-6 px-5">
+              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                <p href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                  Pricing
+                </p>
+
+                <p href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                  Docs
+                </p>
+                {resources.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div>
+                <p className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
+                  Sign up
+                </p>
+                <p className="mt-6 text-center text-base font-medium text-gray-500">
+                  Existing customer?{' '}
+                  <p className="text-indigo-600 hover:text-indigo-500">
+                    Sign in
+                  </p>
+                </p>
+              </div>
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  )
 }
 
 export default App;
