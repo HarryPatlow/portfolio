49d3b6ed11a6b1d1fa5b592e2e3207b03dd5d8fb
Harry Patlow <ghp.coding@gmail.com>
2022-09-08 06:03:25 -0400

    Finish Coding Skills Section 2

--- src/components/adads.js
+++
@@ -1,7 +0,0 @@
-<div class = "mx-16 mb-24">
-        <div class="progress">
-            <div class={`progress-bar bg${mang["wp"]["color"]}`} role="progressbar" aria-label="Segment one" style={{width: ((mang["wp"]["projects"]/(mang["cp"]["projects"]+mang["wp"]["projects"]+mang["TS"]["projects"]))*100).toString()+"%"}} aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
-            <div class={`progress-bar bg${mang["cp"]["color"]}`}  role="progressbar" aria-label="Segment two" style={{width: ((mang["cp"]["projects"]/(mang["cp"]["projects"]+mang["wp"]["projects"]+mang["TS"]["projects"]))*100).toString()+"%"}} aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
-            <div class={`progress-bar bg${mang["webmin"]["color"]}`}  aria-label="Segment three" style={{width:  ((mang["webmin"]["projects"]/(mang["cp"]["projects"]+mang["wp"]["projects"]+mang["TS"]["projects"]))*100).toString()+"%"}} aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
-</div>
